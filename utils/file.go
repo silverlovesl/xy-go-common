94189af455428b9422389c81433e8e36fbff8ef5
XY <541075770@qq.com>
2019-12-03 22:47:12 +0900

    add utils [file]

---
+++ utils/file.go
@@ -0,0 +1,12 @@
+package utils
+
+import "os"
+
+// FileExists decect if file  exists
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
